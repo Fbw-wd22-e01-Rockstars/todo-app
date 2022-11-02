c80e52d56d7191b414b3a9480facd05ebd1fb0af
OpheLiaxx <ophechan@gmail.com>
2022-11-02 22:06:51 +0100

    amend css

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,35 +1,43 @@
-import React from 'react'
-import {Link, BrowserRouter as router, Router} from "react-router-dom"
+import React from "react";
+import { Link} from "react-router-dom";
 function NavBar() {
-    return (
-        <div>
-     
-
-            <ul style={{display:"flex", listStyle:"none", justifyContent:"space-evenly" , width:"100vw", position:"absolute", top:0, left:0, backgroundColor:"white" }}>
-                <li>
-                    <Link to="/"  style={{textDecoration:"none"}}>
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/signin"  style={{textDecoration:"none"}}>
-                        Signin
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/signup"  style={{textDecoration:"none"}}>
-                        Signup
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/dashboard"  style={{textDecoration:"none"}}>
-                        Dashboard
-                    </Link>
-                </li>
-            </ul>
-  
-        </div>
-    )
+  return (
+    <div>
+      <ul
+        style={{
+          display: "flex",
+            justifyContent: "space-around",
+            listStyle: "none",
+            backgroundColor: "lightblue",
+            padding: "10px",
+            width: "100vw",
+            hover: "red",
+            top: "0",
+        }}
+        >
+        <li>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/signin" style={{ textDecoration: "none" }}>
+            Signin
+          </Link>
+        </li>
+        <li>
+          <Link to="/signup" style={{ textDecoration: "none" }}>
+            Signup
+          </Link>
+        </li>
+        <li>
+          <Link to="/dashboard" style={{ textDecoration: "none" }}>
+            Dashboard
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
