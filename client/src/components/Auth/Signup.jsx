191d67ec8fc44d52b39ad7a59d9fe1b637cadc25
Neetu <upneetneetu@gmail.com>
2022-11-03 11:55:44 +0100

    it worked and then it didn't.

--- client/src/components/Auth/Signup.jsx
+++ client/src/components/Auth/Signup.jsx
@@ -1,50 +1,72 @@
 import React, { useState } from 'react'
 import axios from "axios"
 
-function Signup() {
+
+
+export default function Signup() {
 
     const [userData, setUserData] = useState({
-        email:"",
-        password:"",
-        name:""
+        email: "",
+        password: "",
+        name: ""
     })
 
+    const [message, setMessage] = useState(false);
+    const [failMessage, setFailMessage] = useState(false);
 
 
-    const submitHandler = (e) =>{
+    const submitHandler = (e) => {
         e.preventDefault()
         setUserData({
-            name:e.target.name.value,
-            email:e.target.email.value,
-            password:e.target.password.value
+            name: e.target.name.value,
+            email: e.target.email.value,
+            password: e.target.password.value
         })
-        axios.post(`${process.env.REACT_APP_BE_URL}/auth/signup`,userData)
-    .then(res=>console.log("response from backend", res))
-    .catch(err => console.log(err))
-
-    }
+        
+        axios
+            .post(`${process.env.REACT_APP_BE_URL}/auth/signup`, userData)
+            .then((res) => {
+                setFailMessage(false);
+                setMessage(true);
+            })
+            .catch((err) => {
+                if (err.response.data.status === "failed") {
+                    setMessage(false);
+                    setFailMessage(true);
+                }
+            });
+    };
+    
     return (
         <div>
             <h3>Register</h3>
             <form action='post' onSubmit={submitHandler}>
                 <div>
-                <div>
-                <label htmlFor="name">Name   :  </label>
-                <input type="text" id="name" placeholder="enter your name" name="name" />
-                </div>
-                <label htmlFor="email">Email   :  </label>
-                <input type="email" id="email" placeholder="enter your email" name="email"/>
-                </div>
-
-                <div>
-                <label htmlFor="password">password   :  </label>
-                <input type="password" id="password" placeholder="enter your password" name="password" />
+                    <div>
+                        <label htmlFor="name">Name   :  </label>
+                        <input type="text" id="name" placeholder="enter your name" name="name" />
+                    </div>
+                    <label htmlFor="email">Email   :  </label>
+                    <input type="email" id="email" placeholder="enter your email" name="email" />
                 </div>
 
-                <button type='submit'> Signup</button>
+                <div>
+                    <label htmlFor="password">password   :  </label>
+                    <input type="password" id="password" placeholder="enter your password" name="password" />
+                </div>
+
+                <button type='submit'> Sign Up!</button>
             </form>
+            <hr />
+            {message ? (
+                <h3 style={{ color: "green" }}>Sign up successful! </h3>
+            ) : ""}
+            {failMessage ? (
+                <h3 style={{ color: "red" }}>Invalid input, sign up unsuccessful.</h3>
+            ) : ""}
         </div>
     )
-}
+            
 
-export default Signup
+
+}
--- controllers/authController.js
+++ controllers/authController.js
@@ -3,14 +3,17 @@ import bcrypt from "bcrypt"
 export const signupController = async (req, res) =>{
     
     // 1. I will search my DB either user is already registered
-console.log(req.body)
+
    const {email, password, name} = req.body
 
    const foundUser = await User.findOne({email})
    
    // 2. if a user is already registered with the same email address we will throw an error
 
-   if(foundUser) return res.status(401).json({status:"failed", message:"email already registered"})
+    if (foundUser)
+        return res
+            .status(401)
+            .json({ status: "failed", message: "email already registered" })
    
    // Hash the password before saving on database
 
