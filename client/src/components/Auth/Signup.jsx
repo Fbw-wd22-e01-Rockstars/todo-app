b42f1e0eec69c938e0401aad78681b7ea6608fb3
Neetu <upneetneetu@gmail.com>
2022-11-03 13:42:29 +0100

    added json token

--- client/src/components/Auth/Signup.jsx
+++ client/src/components/Auth/Signup.jsx
@@ -5,67 +5,102 @@ import axios from "axios"
 
 export default function Signup() {
 
-    const [userData, setUserData] = useState({
-        email: "",
-        password: "",
-        name: ""
-    })
+    // const [userData, setUserData] = useState({
+    //     email: "",
+    //     password: "",
+    //     name: ""
+    // })
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const [message, setMessage] = useState(false);
     const [failMessage, setFailMessage] = useState(false);
 
-
+    const userData = { name, email, password };
     const submitHandler = (e) => {
-        e.preventDefault()
-        setUserData({
-            name: e.target.name.value,
-            email: e.target.email.value,
-            password: e.target.password.value
-        })
+        // e.preventDefault()
+        // setUserData({
+        //     name: e.target.name.value,
+        //     email: e.target.email.value,
+        //     password: e.target.password.value
+        // })
         
         axios
             .post(`${process.env.REACT_APP_BE_URL}/auth/signup`, userData)
             .then((res) => {
                 setFailMessage(false);
                 setMessage(true);
+
+                setName("")
+                setEmail("")
+                setPassword("")
             })
             .catch((err) => {
                 if (err.response.data.status === "failed") {
                     setMessage(false);
                     setFailMessage(true);
+
+                    setEmail("")
                 }
             });
     };
     
     return (
-        <div>
-            <h3>Register</h3>
-            <form action='post' onSubmit={submitHandler}>
-                <div>
-                    <div>
-                        <label htmlFor="name">Name   :  </label>
-                        <input type="text" id="name" placeholder="enter your name" name="name" />
-                    </div>
-                    <label htmlFor="email">Email   :  </label>
-                    <input type="email" id="email" placeholder="enter your email" name="email" />
-                </div>
+      <div>
+        <h3>Register</h3>
+        <form action="post" onSubmit={submitHandler}>
+          <div>
+            <div>
+              <label htmlFor="name">Name : </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="enter your name"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <label htmlFor="email">Email : </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="enter your email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-                <div>
-                    <label htmlFor="password">password   :  </label>
-                    <input type="password" id="password" placeholder="enter your password" name="password" />
-                </div>
+          <div>
+            <label htmlFor="password">password : </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="enter your password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-                <button type='submit'> Sign Up!</button>
-            </form>
-            <hr />
-            {message ? (
-                <h3 style={{ color: "green" }}>Sign up successful! </h3>
-            ) : ""}
-            {failMessage ? (
-                <h3 style={{ color: "red" }}>Invalid input, sign up unsuccessful.</h3>
-            ) : ""}
-        </div>
-    )
+          <button type="submit"> Sign Up!</button>
+        </form>
+        <hr />
+        {message ? (
+          <h3 style={{ color: "green" }}>Sign up successful! </h3>
+        ) : (
+          ""
+        )}
+        {failMessage ? (
+          <h3 style={{ color: "red" }}>Invalid input, please try again.</h3>
+        ) : (
+          ""
+        )}
+      </div>
+    );
             
 
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,9 @@
 import User from "../models/userModel.js"
 import bcrypt from "bcrypt"
-export const signupController = async (req, res) =>{
+import jwt from "jsonwebtoken"
+
+
+export const signupController = async (req, res) => {
     
     // 1. I will search my DB either user is already registered
 
@@ -15,7 +18,8 @@ export const signupController = async (req, res) =>{
             .status(401)
             .json({ status: "failed", message: "email already registered" })
    
-   // Hash the password before saving on database
+    
+    // 3. if user is not registered we will hash the password and save the user in DB
 
    const saltRound = 10
    const salt = await bcrypt.genSalt(saltRound)
@@ -28,16 +32,37 @@ export const signupController = async (req, res) =>{
 
 }
 
-export const signinController = async (req, res) =>{
+//4. I will create a web token and send it to the client
 
-    console.log(req.body)
-    const  {email, password} = req.body
-    
-    const currentUser = await User.findOne({email})
-    if(!currentUser) return res.status(400).json({status: "failed", message: "Invalid Credentials"})
-
-    const verified = await bcrypt.compare(password, currentUser.password)
-
-    if(!verified) return res.status(400).json({status: "failed", message: "Invalid Credentials"})
-    res.status(200).json({status: "success", data: {email : currentUser.email, name: currentUser.name}})
+export const signinController = async (req, res) => {
+    const { email, password } = req.body
+    const foundUser = await User.findOne({ email })
+    if (!foundUser)
+        return res
+            .status(401)
+            .json({ status: "failed", message: "email not registered" })
+    const isPasswordCorrect = await bcrypt.compare(password, foundUser.password)
+    if (!isPasswordCorrect)
+        return res
+            .status(401)
+            .json({ status: "failed", message: "password is incorrect" })
+    const token = jwt.sign({ id: foundUser._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+    })
+    res.status(200).json({ status: "success", token })
 }
+
+//5. I will create a middleware to protect my routes
+
+export const protectRoute = async (req, res, next) => {
+    const token = req.headers.authorization.split(" ")[1]
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+    const foundUser = await User.findById(decodedToken.id)
+    if (!foundUser)
+        return res
+            .status(401)
+            .json({ status: "failed", message: "user not found" })
+    req.user = foundUser
+    next()
+}
+
